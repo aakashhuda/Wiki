144624da66f51fbe590469d20a035dc834f26143
aakashhuda <aakash.huda@gmail.com>
2020-12-26 22:01:40 +0600

    Search,Entry functions working. Added Bootstrap styling.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.get, name = "get"),
     path("search", views.search, name = "search"),
+    path("create_page", views.new_page, name = "new_page"),
+    path("add_entry", views.add_entry, name = "add_entry"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from markdown2 import Markdown
+from django.urls import reverse
+from django.http import HttpResponseRedirect,HttpResponse
+from django import forms
 import re
 
 from . import util
+
+class NewEntryForm(forms.Form):
+    title = forms.CharField(required=True, label="Enter title ",max_length=20,min_length=1,widget=forms.TextInput(attrs={"placeholder":"Title","class":"form-control w-50 font-italic"}))
+    blog = forms.CharField(widget=forms.Textarea(attrs={"placeholder":"Type markdown content here...","class":"form-control w-75"}))
 
 
 def index(request):
@@ -25,19 +32,13 @@ def get(request,title):
 def search(request):
     if request.method == 'POST':
         s_result = []
-        m = Markdown()
         q = request.POST["q"]
         q_lower = q.lower()
         entries = util.list_entries()
         pattern = re.compile(q_lower)
         for entry in entries:
             if q.lower()==entry.lower():
-                f = util.get_entry(entry)
-                content = m.convert(f)
-                return render(request, "encyclopedia/blog.html", {
-                    "content":content,
-                    "title":entry,
-                })
+                return HttpResponseRedirect(reverse('encyclopedia:get',kwargs={"title":entry}))
             else:
                 match = pattern.search(entry.lower())
                 if match is None:
@@ -48,3 +49,28 @@ def search(request):
         return render(request, "encyclopedia/search.html", {
             "s_result":s_result,
         })
+def new_page(request):
+    return render(request, "encyclopedia/new_page.html",{
+        "form": NewEntryForm()
+    })
+def add_entry(request):
+    if request.method =="POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            blog = form.cleaned_data["blog"]
+            entries = util.list_entries()
+            for entry in entries:
+                if title.lower()==entry.lower():
+                    return render(request, "encyclopedia/error.html", {
+                        "heading": "ERROR",
+                        "message": "This title already exists!"
+                    })
+                else:
+                    continue
+            util.save_entry(title,blog)
+            return HttpResponseRedirect(reverse('encyclopedia:index'))
+        else:
+            return render(request, "encyclopedia/new_page.html",{
+                "form": form,
+            })
